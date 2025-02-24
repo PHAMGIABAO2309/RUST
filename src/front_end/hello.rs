a94c36603faac547976bee7bb8ed3714e4b76fb7
Daniel <hieutiti1992103@gmail.com>
2025-02-24 14:52:08 +0700

    Auto commit from cargo run

--- src/front_end/hello.rs
+++ src/front_end/hello.rs
@@ -5,7 +5,6 @@ pub fn home(poem_content: &str, my_func_content: &str) -> String {
 <head>
   <meta charset="utf-8">
   <title>Hello from Rust!</title>
-  <link rel="stylesheet" href="/static/index.css">
 </head>
 <body>
   <h1>Tôi là Phạm Gia Bảo! Tôi đang test Localhost</h1>
