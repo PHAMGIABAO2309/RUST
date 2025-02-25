41c968e8c678ccb3564fecc3cccdc3f2c574f2f7
Daniel <hieutiti1992103@gmail.com>
2025-02-25 21:59:56 +0700

    Auto commit from cargo run

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,11 @@ use tokio::sync::Mutex;
 
 #[tokio::main]
 async fn main() {
-    let pool = database::connect_db().await.expect("Không thể kết nối MySQL");  // 👉 Kết nối database
-    let chapter_name = "NGHỊ ĐỊNH"; // Hoặc lấy từ config
-    let poem_content = route::get_poem_data(&pool, chapter_name).await;   // 👉 Lấy dữ liệu thơ từ database
-
+    let pool = database::connect_db().await.expect("Không thể kết nối MySQL");  // 👉 Kết nối database 
+    let poem_content = route::get_poem_data(&pool, "NGHỊ ĐỊNH").await;   // 👉 Lấy dữ liệu thơ từ database
     // 👉 Chuyển đổi dữ liệu để phù hợp với `create_hello_route`
     let poem = Arc::new(Mutex::new((
-        chapter_name.to_string(),
+        "NGHỊ ĐỊNH".to_string(),
         "".to_string(),
         "".to_string(),
         poem_content.lock().await.clone(),
