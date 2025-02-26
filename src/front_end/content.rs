890ae8171e7132971c80d65377a779806c37a503
Daniel <hieutiti1992103@gmail.com>
2025-02-26 18:01:25 +0700

    Auto commit from cargo run

--- src/front_end/content.rs
+++ src/front_end/content.rs
@@ -9,30 +9,22 @@ pub async fn get_document_content(pool: &MySqlPool) -> Result<String, sqlx::Erro
     )
     .fetch_all(pool)
     .await?;
-
     let mut html_content = String::new();
-
     for row in rows {
         let title: String = row.get("Title");
         let content: String = row.get("Content");
-
         html_content.push_str(&document_content(&title, &content));
     }
-
     Ok(html_content)
 }
-
 pub fn document_content(title: &str, content: &str) -> String {
     let bold_regex = Regex::new(r"(Điều \d+\..*?)\r\n").unwrap();
-    
-    // Bôi đậm các dòng "Điều X."
+     // Bôi đậm các dòng "Điều X."
     let formatted_content = bold_regex.replace_all(content, |caps: &regex::Captures| {
         format!("<strong>{}</strong><br><br>", &caps[1])
     });
-
     // Chuyển đổi xuống dòng `\r\n` thành `<br>` để hiển thị đúng trên HTML
     let formatted_content = formatted_content.replace("\r\n", "<br><br>");
-
     format!(
         r#"
     <div class="content-section">
